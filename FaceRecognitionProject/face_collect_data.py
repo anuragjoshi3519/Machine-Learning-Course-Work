012a2b13f37b3572f8394dcfb1eef9e2849e923f
anuragjoshi3519 <anurag_joshi@outlook.com>
2019-09-19 14:03:37 +0530

    Support Vector Machine Model added

--- FaceRecognitionProject/face_collect_data.py
+++ FaceRecognitionProject/face_collect_data.py
@@ -11,26 +11,26 @@ while True:
     if ret==False:
         continue
     cv2.imshow('Frame',frame)
+    
     faces=face_cascade.detectMultiScale(frame,1.3,5)
     faces=sorted(faces,key=lambda f: f[2]*f[3])
-    
+
     if len(faces)==0:
         continue
-        
+
     for x,y,w,h in faces[-1:]:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
-    
+
     # Taking frames into the imageData file
     offset=8
     frame_crop=frame[y-offset:y+h+offset,x-offset:x+w+offset]
     frame_crop=cv2.resize(frame_crop,(100,100))
     frame_crop=cv2.cvtColor(frame_crop,cv2.COLOR_BGR2GRAY)
-    
+  
     if stop%5==0:
         imageData.append(frame_crop)
         print(len(imageData))
     stop+=1
-    
     
     cv2.imshow('Face',frame_crop)
     
