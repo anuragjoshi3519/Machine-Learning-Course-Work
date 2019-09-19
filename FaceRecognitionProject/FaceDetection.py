72bbe81a49144420ed79e96d476e47ea4f2e75f5
anuragjoshi3519 <anurag_joshi@outlook.com>
2019-09-20 03:14:49 +0530

    Merger

--- FaceRecognitionProject/FaceDetection.py
+++ FaceRecognitionProject/FaceDetection.py
@@ -1,7 +1,7 @@
 import cv2
 cap=cv2.VideoCapture(0)
 
-face_cascade=cv2.CascadeClassifier('FaceRecognitionProject/haarcascade_frontalface_alt.xml')
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 while True:
     ret,frame=cap.read()
